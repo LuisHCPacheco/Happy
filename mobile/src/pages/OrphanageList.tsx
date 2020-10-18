ff59b200dcd68c6096f87d3383eecf171bec2e53
Luis Pacheco <luishenriquepacheco184@gmail.com>
2020-10-18 17:19:57 -0300

    Mobile1.2

--- mobile/src/pages/OrphanageList.tsx
+++ mobile/src/pages/OrphanageList.tsx
@@ -58,11 +58,6 @@ export default function OrphanageDetails() {
           )
         })}
       </View>
-
-        {/* <RectButton style={styles.contactButton} onPress={() => {}}>
-          <FontAwesome name="whatsapp" size={24} color="#FFF" />
-          <Text style={styles.contactButtonText}>Entrar em contato</Text>
-        </RectButton> */}
     </ScrollView>
   )
 }
@@ -87,7 +82,8 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontFamily: 'Nunito_700Bold',
 
-    marginLeft: 10,
+    marginLeft: 5,
+    marginTop: 5,
   },
 
   wppButton:{
@@ -119,105 +115,5 @@ const styles = StyleSheet.create({
     color: '#5c8599',
     lineHeight: 24,
     marginTop: 16,
-  },
-
-  mapContainer: {
-    borderRadius: 20,
-    overflow: 'hidden',
-    borderWidth: 1.2,
-    borderColor: '#B3DAE2',
-    marginTop: 40,
-    backgroundColor: '#E6F7FB',
-  },
-
-  mapStyle: {
-    width: '100%',
-    height: 150,
-  },
-
-  routesContainer: {
-    padding: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  routesText: {
-    fontFamily: 'Nunito_700Bold',
-    color: '#0089a5'
-  },
-
-  separator: {
-    height: 0.8,
-    width: '100%',
-    backgroundColor: '#D3E2E6',
-    marginVertical: 40,
-  },
-
-  scheduleContainer: {
-    marginTop: 24,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-
-  scheduleItem: {
-    width: '48%',
-    padding: 20,
-  },
-
-  scheduleItemBlue: {
-    backgroundColor: '#E6F7FB',
-    borderWidth: 1,
-    borderColor: '#B3DAE2',
-    borderRadius: 20,
-  },
-
-  scheduleItemGreen: {
-    backgroundColor: '#EDFFF6',
-    borderWidth: 1,
-    borderColor: '#A1E9C5',
-    borderRadius: 20,
-  },
-
-  scheduleItemRed: {
-    backgroundColor: '#FDF0F5',
-    borderWidth: 1,
-    borderColor: '#FFBCD4',
-    borderRadius: 20,
-  },
-
-  scheduleText: {
-    fontFamily: 'Nunito_600SemiBold',
-    fontSize: 16,
-    lineHeight: 24,
-    marginTop: 20,
-  },
-
-  scheduleTextBlue: {
-    color: '#5C8599'
-  },
-
-  scheduleTextGreen: {
-    color: '#37C77F'
-  },
-
-  scheduleTextRed: {
-    color: '#FF669D'
-  },
-
-  contactButton: {
-    backgroundColor: '#3CDC8C',
-    borderRadius: 20,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 56,
-    marginTop: 40,
-  },
-
-  contactButtonText: {
-    fontFamily: 'Nunito_800ExtraBold',
-    color: '#FFF',
-    fontSize: 16,
-    marginLeft: 16,
   }
 })
